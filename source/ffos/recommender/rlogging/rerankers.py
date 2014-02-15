7104714df21b2f31099f2791dbfc7dde8261d61c
joaonrb <joaonrb@gmail.com>
2014-02-15 16:16:04 +0100

    [IMPROVEMENT] Big change in the calculation. Makes the changes between recommendations more smooth

--- source/ffos/recommender/rlogging/rerankers.py
+++ source/ffos/recommender/rlogging/rerankers.py
@@ -12,7 +12,6 @@ __author__ = "joaonrb"
 from ffos.recommender.filters import ReRanker
 from ffos.recommender.rlogging.models import RLog
 from django.db.models import Count, Sum
-import numpy as np
 
 MAX_LOG = 120000
 
@@ -39,78 +38,16 @@ class SimpleLogReRanker(ReRanker):
     way the changes will be smother.
     """
 
-    def __init__(self, constant=2, gravity_point=np.mean):
+    def __init__(self, constant=10, rank_calculator=lambda rank, mean, count, size: rank + ((size - mean) ** count)):
         """
         Constructor
 
-        :param constant: Locator for the re-ranker. If set to 0 the re ranker produce the same recommendation as the
-         input. Default is 1.
-        :type constant: int.
-        :type constant: float.
-        :param gravity_point: A callable object to calculate the gravity point. It should receive an array of numbers
+        :param rank_calculator: A callable object to calculate the gravity point. It should receive an array of numbers
          and return a gravity point.
-        :type gravity_point: collections.Callable.
+        :type rank_calculator: collections.Callable.
         """
+        self._rank_calculator = rank_calculator
         self._constant = constant
-        self._gravity_point = gravity_point
-
-    def __call_prototype__(self, user, app_score):
-        """
-        I will do this documentation latter
-        """
-        # Get the last "MAX_LOG"s logs ordered by timestamp
-        logs = RLog.objects.filter(item__id__in=app_score, user=user).order_by("-timestamp").values_list(
-            "item__pk", "type", "value", "timestamp")[:MAX_LOG]
-
-        # Since there Django is not kind enough to map the entries, I guess I do it myself. I some one knows a way that
-        # I'm not aware of, just change it and ask for merge.
-        mapped_logs = {}
-        for item, log_type, value, timestamp in logs:
-            try:
-                recommended_sum, log_list = mapped_logs[item]
-
-            except KeyError:
-                recommended_sum, log_list = 0, []
-            mapped_logs[item] = \
-                recommended_sum + 1 if log_type == RLog.RECOMMEND else recommended_sum, \
-                log_list + [(item, log_type, value, timestamp)]
-
-        # Calculate the gravity point for each item
-        tmp_mapped_logs = {}
-        for item, (recommended_sum, log_list) in mapped_logs.iteritems():
-            # Get the recommended logs for this item in a generator
-            values = (value for _, log_type, value, _ in log_list if log_type == RLog.RECOMMEND)
-
-            # Calculate the gravity point
-            try:
-                gravity_point = self._gravity_point(values)
-            except AttributeError or ZeroDivisionError:
-                gravity_point = \
-                    sum((value for _, log_type, value, _ in log_list if log_type == RLog.RECOMMEND)) / len(log_list)
-
-            # Add it to the mapped items
-            tmp_mapped_logs[item] = gravity_point, recommended_sum, log_list
-        mapped_logs = tmp_mapped_logs
-
-        # Now we ready. Lets re-rank them
-
-        # First get all the variables by their app rank (GENERATOR)
-        ranked_variables = enumerate(((e, mapped_logs[e] if e in mapped_logs.keys() else (i, 0, []))
-                                      for i, e in enumerate(app_score, start=1)), start=1)
-
-        # Than we calculate the new rank for each app based on new scores (new rank, item)
-        new_scores = (((len(app_score)/gravity_point)**recommended_sum, item)
-                      for rank, (item, (gravity_point, recommended_sum, log_list)) in ranked_variables)
-
-        # Now we just nee to sort them by the new rank and send an "item_id only" list
-        result = [item_id for _, item_id in sorted(new_scores, cmp=lambda x, y: cmp(x[0], y[0]))]
-
-        # Just a quick check in the result, see if it makes sense
-        assert result != app_score or len(RLog.objects.filter(item__id__in=app_score, user=user, type=0)) == 0, \
-            "This isn't doing anything"
-        assert len(result) == len(app_score), \
-            "Some one was messing with the result len (original:%s, result:%s" % (len(app_score), len(result))
-        return result
 
     def __call__(self, user, early_recommendation):
         """
@@ -150,7 +87,7 @@ class SimpleLogReRanker(ReRanker):
             try:
                 mean = sum_value/count
                 #new_rank = (number_of_apps / mean) ** count
-                new_rank = rank - mean ** count
+                new_rank = self._rank_calculator(rank, mean, count, self._constant if mean < self._constant else mean)
             except ZeroDivisionError:
                 new_rank = rank
             #print new_rank
@@ -159,6 +96,4 @@ class SimpleLogReRanker(ReRanker):
         # We just need to sort
         result = [item_id for _, item_id in sorted(new_scores, cmp=lambda x, y: cmp(x[0], y[0]))]
 
-        #assert result == self.__call_prototype__(user, early_recommendation), \
-        #    "Call and call_prototype are not producing the same result"
         return result
