e02e8f6a254269ca57962bf0c314cbcf89ef4344
joaonrb <joaonrb@gmail.com>
2014-04-08 11:30:26 +0200

    Tests for filter owned items

--- src/recommendation/filter_owned/tests.py
+++ src/recommendation/filter_owned/tests.py
@@ -4,3 +4,33 @@ Tests for the language package
 
 __author__ = "joaonrb"
 
+
+from recommendation.filter_owned.filters import FilterOwnedFilter
+
+
+class DummyUser(object):
+    class owned_items:
+        @staticmethod
+        def all():
+            class dummy:
+                @staticmethod
+                def values_list(dummy_string):
+                    return [(1,), (4,)]
+            return dummy
+
+
+class TestFilteredOwned(object):
+    """
+    Test suit for filter plugin to owned items
+    """
+
+    @staticmethod
+    def test_owned_items_are_filtered():
+        """
+        Test if the dummy user items are filtered to minus infinity
+        """
+        user = DummyUser()
+        recommendation = [.4, .2, .7, .1, .3, .4]
+        filtered_recommendation = FilterOwnedFilter()(user, recommendation)
+        assert filtered_recommendation[0] == float("-inf"), "First item in recommendation is not -inf"
+        assert filtered_recommendation[3] == float("-inf"), "Fourth item in recommendation is not -inf"
