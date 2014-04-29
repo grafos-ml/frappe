8b9de636c70c54e862a7a110794abe20db057a2b
joaonrb <joaonrb@gmail.com>
2014-04-29 12:19:07 +0200

    Change in java Tensor Cofi for python by file

--- java/src/main/java/es/tid/frappe/python/TensorCoPy.java
+++ java/src/main/java/es/tid/frappe/python/TensorCoPy.java
@@ -10,7 +10,6 @@ import java.util.List;
 import org.jblas.FloatMatrix;
 
 import es.tid.frappe.recsys.TensorCoFi;
-import es.tid.frappe.recsys.TFThreads;
 
 public class TensorCoPy {
 	
@@ -21,22 +20,36 @@ public class TensorCoPy {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
+        String directory = System.getProperty("directory");
+        int number_of_factors = Integer.valueOf(System.getProperty("n_factors"));
+        int number_of_iterations = Integer.valueOf(System.getProperty("n_iterations"));
+        float lambda = Float.valueOf(System.getProperty("lambda"));
+        float alpha = Float.valueOf(System.getProperty("alpha"));
+        int number_of_contexts = Integer.valueOf(System.getProperty("n_contexts"));
+        String contexts[] = new String[number_of_contexts];
+        int dimensions[] = new int[number_of_contexts];
+        for(int i=0; i<number_of_contexts; i++) {
+            contexts[i] = System.getProperty("context" + i);
+            dimensions[i] = Integer.valueOf(System.getProperty("dimension" + i));
+        }
+        String path;
+
+        // Stop the output
 		PrintStream out = System.out;
 		System.setOut(new PrintStream(new OutputStream() {
 		   @Override public void write(int b) throws IOException {}
 		 }));
-		FloatMatrix data = FloatMatrix.loadCSVFile(args[0]+"train.csv");
-		TensorCoFi ts = new TensorCoFi(Integer.valueOf(args[1]), 
-				Integer.valueOf(args[2]), Float.valueOf(args[3]),
-				Float.valueOf(args[4]),new int[]{Integer.valueOf(args[5]),
-			Integer.valueOf(args[6])});
+		FloatMatrix data = FloatMatrix.loadCSVFile(directory+"train.csv");
+		TensorCoFi ts = new TensorCoFi(number_of_factors, number_of_iterations, lambda, alpha, dimensions);
 		ts.train(data);
 		List<FloatMatrix> model = ts.getModel();
-		floatMatrixToCSV(model.get(0),args[0]+"user.csv");
-		floatMatrixToCSV(model.get(1),args[0]+"item.csv");
-		System.setOut(out);
-		System.out.print(args[0]+"user.csv ");
-		System.out.print(args[0]+"item.csv");
+        // Start the output
+        System.setOut(out);
+        for(int i=0; i<number_of_contexts; i++) {
+            path = directory + contexts[i] +".csv";
+            floatMatrixToCSV(model.get(i), path);
+            System.out.print(path + (i == number_of_contexts-1 ? "" : " "));
+        }
 	}
 	
 	private static void floatMatrixToCSV(FloatMatrix fm, String fpath) {
@@ -44,7 +57,7 @@ public class TensorCoPy {
 		for(int row = 0; row < fm.rows; row++) {
 			for(int column = 0; column < fm.columns; column++)
 				sb.append(String.valueOf(fm.get(row,column)))
-					.append((column+1==fm.columns)?"":",");
+					.append((column+1==fm.columns) ? "" : ",");
 			sb.append('\n');
 		}
 		
