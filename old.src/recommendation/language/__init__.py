c99bceb411c2cf42aba57c8b0308759824503f5b
João Nuno <joaonrb@gmail.com>
2014-09-12 12:09:26 +0200

    Make documentation

--- old.src/recommendation/language/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- encofing=utf-8 -*-
-"""
-This is the locale plugin. This feature adds languages support along with some neat filter so you can add for processing
-the recommendation.
-"""
-
-__author__ = "joaonrb"
--- old.src/recommendation/records/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-
-
-.. moduleauthor:: joaonrb <joaonrb@gmail.com>
-"""
-__author__ = 'joaonrb'
