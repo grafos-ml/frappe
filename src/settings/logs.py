125c03ffd9a683e2924644508829698ded57f3ae
João Nuno <joaonrb@gmail.com>
2014-11-26 11:40:43 +0100

    [Feature] settings package. - Add logs module

---
+++ src/settings/logs.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+frappe - settings
+joaonrb, 26 November 2014
+
+Logging settings.
+"""
+
+from __future__ import division, absolute_import, print_function
+import logging
+
+__author__ = "joaonrb"
+
+SYSLOG_TAG = "http_app_frappe"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "root": {
+        "level": "WARNING",
+        "handlers": ["sentry"],
+    },
+    "formatters": {
+        "simple": {
+            "format": "%(asctime)s %(name)s:%(levelname)s %(hostname)s {0}: "
+                      "%(message)s :%(pathname)s:%(lineno)s".format(SYSLOG_TAG)
+        },
+    },
+    "handlers": {
+        "syslog": {
+            "facility": logging.handlers.SysLogHandler.LOG_LOCAL7,
+            "formatter": "simple",
+         },
+        "sentry": {
+            "level": "ERROR",
+            "class": "raven.contrib.django.raven_compat.handlers.SentryHandler",
+            },
+        },
+    "loggers": {
+        "django.db.backends": {
+            "level": "ERROR",
+            "handlers": ["console", "syslog"],
+            "propagate": False,
+        },
+        "raven": {
+            "level": "DEBUG",
+            "handlers": ["console", "syslog"],
+            "propagate": False,
+            },
+        "sentry.errors": {
+            "level": "DEBUG",
+            "handlers": ["console", "syslog"],
+            "propagate": False,
+        },
+    },
+}
