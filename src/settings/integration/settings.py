08b906137207dcb99dd082f7f13cef5003b96a45
João Nuno <joaonrb@gmail.com>
2014-11-26 12:40:00 +0100

    Add integration settings

---
+++ src/settings/integration/settings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+frappe - settings.integration
+joaonrb, 26 November 2014
+
+Integration test settings
+"""
+
+from __future__ import division, absolute_import, print_function
+from settings.base import *
+
+__author__ = "joaonrb"
