232b3a58b28e0ce04086c2c5eff400be554db8ba
João Nuno <joaonrb@gmail.com>
2014-11-27 14:18:11 +0100

    Change wsgi file

--- src/recommendation/settings/wsgi.py
+++ src/recommendation/settings/wsgi.py
@@ -9,24 +9,26 @@ from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
 
+from threading import Thread
 from django.conf import settings
 from recommendation.models import Item, User, TensorCoFi, Popularity
 
+
+def load_rest():
+    User.load_to_cache()
+
+    # Load main models
+    Popularity.load_to_cache()
+    TensorCoFi.load_to_cache()
+    if "recommendation.language" in settings.INSTALLED_APPS:
+        from recommendation.language.models import Region
+        Region.load_to_cache()
+    if "recommendation.diversity" in settings.INSTALLED_APPS:
+        from recommendation.diversity.models import ItemGenre, Genre
+        Genre.load_to_cache()
+        ItemGenre.load_to_cache()
+
+
 # Load user and items
 Item.load_to_cache()
-User.load_to_cache()
-
-# Load main models
-Popularity.load_to_cache()
-TensorCoFi.load_to_cache()
-
-
-if "recommendation.language" in settings.INSTALLED_APPS:
-    from recommendation.language.models import Locale, Region
-    Locale.load_to_cache()
-    Region.load_to_cache()
-
-if "recommendation.diversity" in settings.INSTALLED_APPS:
-    from recommendation.diversity.models import ItemGenre, Genre
-    Genre.load_to_cache()
-    ItemGenre.load_to_cache()
+Thread(target=load_rest).start()
