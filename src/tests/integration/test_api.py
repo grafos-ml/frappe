e841a8d53e973db9ab0c2e7bba00b367e1368ca8
João Nuno <joaonrb@gmail.com>
2014-12-01 11:36:50 +0100

    [FIX] Add empty test in integration for travis

---
+++ src/tests/integration/test_api.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+frappe - tests.integration
+joaonrb, 01 December 2014
+
+Documentation TODO
+"""
+
+from __future__ import division, absolute_import, print_function
+# The remaining imports here
+
+__author__ = "joaonrb"
+
+
+# Your code
